9c8cb4a583c0f45292f7219a3b81029209fc090d
Alendas67 <sivov_au@mail.ru>
2026-04-24 00:27:41 +0300

    Исправляем кодировку в bat the commit message for your changes. Lines starting

--- run_app.bat
+++ run_app.bat
@@ -1,10 +1,5 @@
 @echo off
-REM Переходим в папку, где лежит bat-файл
 cd /d "%~dp0"
-REM Показываем текущий путь для отладки
-echo Запуск из: %cd%
-
-REM Запускаем Streamlit через модуль Python (надёжнее)
+echo Running from: %cd%
 .\python\python.exe -m streamlit run app.py
-
 pause
