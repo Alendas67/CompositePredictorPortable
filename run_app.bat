088f33058ba53df16ba27cc9484b091729559b64
Alendas67 <sivov_au@mail.ru>
2026-04-24 00:21:37 +0300

    Корректировка дирректории запуска приложения

--- run_app.bat
+++ run_app.bat
@@ -1,6 +1,10 @@
 @echo off
+REM Переходим в папку, где лежит bat-файл
 cd /d "%~dp0"
-start http://localhost:8501
-timeout /t 2 /nobreak >nul
-.\python\Scripts\streamlit.exe run app.py
+REM Показываем текущий путь для отладки
+echo Запуск из: %cd%
+
+REM Запускаем Streamlit через модуль Python (надёжнее)
+.\python\python.exe -m streamlit run app.py
+
 pause
